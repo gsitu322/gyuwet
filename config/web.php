56d433fc432ebe7123b77f7e11f01bb52bb384eb
Gordon <gsitu@sparehire.com>
2018-07-18 17:32:02 -0700

    Got URLs to work

--- config/web.php
+++ config/web.php
@@ -43,14 +43,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<controller:\w+>/<action:\w+>/<id:\d+>'    => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'             => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
